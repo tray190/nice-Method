ad9596e44a761d0a6d91c2ac185dabf9e7e0f8a8
tray190 <belltracy411@gmail.com>
2021-12-03 16:09:14 -0600

    nice method

---
+++ Methods Exercise/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Methods_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite color");
+            string userColor = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite Animal");
+            string animal = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite Artist");
+            string userArtist = Console.ReadLine();
+
+            Console.WriteLine($"There was a guy named { userName} The guy like the color{userColor}");
+            Console.WriteLine($"He likes the {animal} animal and his favorite music artist is {userArtist}");
+ 
+            Console.WriteLine ("Give me a number to add");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Give me a number to add to the first one");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int sum = Sum(num1, num2);
+            Console.WriteLine($"The sum is: {sum}");
+        }
+
+        public static int Sum(int num1, int num2)
+        {
+            return num1 + num2;   
+
+        }
+        public static int multiply (int x, int y)
+        {
+            return x * y;
+        }
+    }
+         
+}
